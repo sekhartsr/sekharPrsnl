78d6a2491bbac6b2c67bcbad466ebde7ce2ff602
Sekhartsr <sekhartsr@gmail.com>
2017-09-03 03:22:24 +0800

    latest changes

--- src/main/java/com/equinix/workvisit/controller/WorkVisitController.java
+++ src/main/java/com/equinix/workvisit/controller/WorkVisitController.java
@@ -1,6 +1,7 @@
 package com.equinix.workvisit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,6 +10,11 @@ public class WorkVisitController {
 	@RequestMapping(value="/",method = RequestMethod.GET)
     public String homepage(){
         return "index";
+    }
+	
+	@RequestMapping(value="/viewDetails/{firstName}",method = RequestMethod.GET)
+    public String viewUserDetails(@PathVariable String firstName){
+        return "viewUser";
     }
 
 }
